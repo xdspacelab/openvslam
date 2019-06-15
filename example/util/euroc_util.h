d5669a4b4ec190fe531ebeec1aacfdcd15f307e5
saweer <51858626+saweer@users.noreply.github.com>
2019-06-16 00:34:21 +0900

    Refactor the utils for EuRoC dataset

--- example/util/euroc_util.h
+++ example/util/euroc_util.h
@@ -1,32 +1,10 @@
-#ifndef EUROC_UTIL_H
-#define EUROC_UTIL_H
+#ifndef EXAMPLE_UTIL_EUROC_UTIL_H
+#define EXAMPLE_UTIL_EUROC_UTIL_H
 
 #include <string>
 #include <vector>
 
-class euroc_mono_sequence {
-public:
-    struct frame {
-        frame(const std::string& img_path, const double timestamp)
-                : img_path_(img_path), timestamp_(timestamp) {};
-
-        const std::string img_path_;
-        const double timestamp_;
-    };
-
-    explicit euroc_mono_sequence(const std::string& seq_dir_path);
-
-    virtual ~euroc_mono_sequence() = default;
-
-    std::vector<frame> get_frames() const;
-
-private:
-    std::vector<double> timestamps_;
-    std::vector<std::string> img_file_paths_;
-    unsigned int num_imgs_;
-};
-
-class euroc_stereo_sequence {
+class euroc_sequence {
 public:
     struct frame {
         frame(const std::string& left_img_path, const std::string& right_img_path, const double timestamp)
@@ -37,9 +15,9 @@ public:
         const double timestamp_;
     };
 
-    explicit euroc_stereo_sequence(const std::string& seq_dir_path);
+    explicit euroc_sequence(const std::string& seq_dir_path);
 
-    virtual ~euroc_stereo_sequence() = default;
+    virtual ~euroc_sequence() = default;
 
     std::vector<frame> get_frames() const;
 
@@ -47,7 +25,6 @@ private:
     std::vector<double> timestamps_;
     std::vector<std::string> left_img_file_paths_;
     std::vector<std::string> right_img_file_paths_;
-    unsigned int num_imgs_;
 };
 
-#endif // EUROC_UTIL_H
+#endif // EXAMPLE_UTIL_EUROC_UTIL_H
