3ad4762ba8032c113ce95ca634905df89f3aaddd
shinsumicco <shinsumicco@gmail.com>
2019-07-24 16:08:03 +0900

    Add examples for a UVC camera

---
+++ example/run_camera_slam.cc
@@ -0,0 +1,190 @@
+#ifdef USE_PANGOLIN_VIEWER
+#include "pangolin_viewer/viewer.h"
+#elif USE_SOCKET_PUBLISHER
+#include "socket_publisher/publisher.h"
+#endif
+
+#include "openvslam/system.h"
+#include "openvslam/config.h"
+
+#include <iostream>
+#include <chrono>
+#include <numeric>
+
+#include <opencv2/core/core.hpp>
+#include <spdlog/spdlog.h>
+#include <popl.hpp>
+
+#ifdef USE_STACK_TRACE_LOGGER
+#include <glog/logging.h>
+#endif
+
+#ifdef USE_GOOGLE_PERFTOOLS
+#include <gperftools/profiler.h>
+#endif
+
+void mono_tracking(const std::shared_ptr<openvslam::config>& cfg,
+                   const std::string& vocab_file_path, const unsigned int cam_num, const std::string& mask_img_path,
+                   const std::string& map_db_path) {
+    // load the mask image
+    const cv::Mat mask = mask_img_path.empty() ? cv::Mat{} : cv::imread(mask_img_path, cv::IMREAD_GRAYSCALE);
+
+    // build a SLAM system
+    openvslam::system SLAM(cfg, vocab_file_path);
+    // startup the SLAM process
+    SLAM.startup();
+
+    // create a viewer object
+    // and pass the frame_publisher and the map_publisher
+#ifdef USE_PANGOLIN_VIEWER
+    pangolin_viewer::viewer viewer(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
+#elif USE_SOCKET_PUBLISHER
+    socket_publisher::publisher publisher(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
+#endif
+
+    auto video = cv::VideoCapture(cam_num);
+    if (!video.isOpened()) {
+        spdlog::critical("cannot open a camera {}", cam_num);
+        SLAM.shutdown();
+        return;
+    }
+
+    cv::Mat frame;
+    double timestamp = 0.0;
+    std::vector<double> track_times;
+
+    unsigned int num_frame = 0;
+
+    bool is_not_end = true;
+    // run the SLAM in another thread
+    std::thread thread([&]() {
+        while (is_not_end) {
+            // check if the termination of SLAM system is requested or not
+            if (SLAM.terminate_is_requested()) {
+                break;
+            }
+
+            is_not_end = video.read(frame);
+            if (frame.empty()) {
+                continue;
+            }
+
+            const auto tp_1 = std::chrono::steady_clock::now();
+
+            // input the current frame and estimate the camera pose
+            SLAM.track_for_monocular(frame, timestamp, mask);
+
+            const auto tp_2 = std::chrono::steady_clock::now();
+
+            const auto track_time = std::chrono::duration_cast<std::chrono::duration<double>>(tp_2 - tp_1).count();
+            track_times.push_back(track_time);
+
+            timestamp += 1.0 / cfg->camera_->fps_;
+            ++num_frame;
+        }
+
+        // wait until the loop BA is finished
+        while (SLAM.loop_BA_is_running()) {
+            std::this_thread::sleep_for(std::chrono::microseconds(5000));
+        }
+    });
+
+    // run the viewer in the current thread
+#ifdef USE_PANGOLIN_VIEWER
+    viewer.run();
+#elif USE_SOCKET_PUBLISHER
+    publisher.run();
+#endif
+
+    thread.join();
+
+    // shutdown the SLAM process
+    SLAM.shutdown();
+
+    if (!map_db_path.empty()) {
+        // output the map database
+        SLAM.save_map_database(map_db_path);
+    }
+
+    std::sort(track_times.begin(), track_times.end());
+    const auto total_track_time = std::accumulate(track_times.begin(), track_times.end(), 0.0);
+    std::cout << "median tracking time: " << track_times.at(track_times.size() / 2) << "[s]" << std::endl;
+    std::cout << "mean tracking time: " << total_track_time / track_times.size() << "[s]" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+#ifdef USE_STACK_TRACE_LOGGER
+    google::InitGoogleLogging(argv[0]);
+    google::InstallFailureSignalHandler();
+#endif
+
+    // create options
+    popl::OptionParser op("Allowed options");
+    auto help = op.add<popl::Switch>("h", "help", "produce help message");
+    auto vocab_file_path = op.add<popl::Value<std::string>>("v", "vocab", "vocabulary file path");
+    auto cam_num = op.add<popl::Value<unsigned int>>("n", "number", "camera number");
+    auto config_file_path = op.add<popl::Value<std::string>>("c", "config", "config file path");
+    auto mask_img_path = op.add<popl::Value<std::string>>("", "mask", "mask image path", "");
+    auto map_db_path = op.add<popl::Value<std::string>>("p", "map-db", "store a map database at this path after SLAM", "");
+    auto debug_mode = op.add<popl::Switch>("", "debug", "debug mode");
+    try {
+        op.parse(argc, argv);
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << std::endl;
+        std::cerr << op << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // check validness of options
+    if (help->is_set()) {
+        std::cerr << op << std::endl;
+        return EXIT_FAILURE;
+    }
+    if (!vocab_file_path->is_set() || !cam_num->is_set()
+        || !config_file_path->is_set()) {
+        std::cerr << "invalid arguments" << std::endl;
+        std::cerr << std::endl;
+        std::cerr << op << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // setup logger
+    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] %^[%L] %v%$");
+    if (debug_mode->is_set()) {
+        spdlog::set_level(spdlog::level::debug);
+    }
+    else {
+        spdlog::set_level(spdlog::level::info);
+    }
+
+    // load configuration
+    std::shared_ptr<openvslam::config> cfg;
+    try {
+        cfg = std::make_shared<openvslam::config>(config_file_path->value());
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+#ifdef USE_GOOGLE_PERFTOOLS
+    ProfilerStart("slam.prof");
+#endif
+
+    // run tracking
+    if (cfg->camera_->setup_type_ == openvslam::camera::setup_type_t::Monocular) {
+        mono_tracking(cfg, vocab_file_path->value(), cam_num->value(), mask_img_path->value(),
+                      map_db_path->value());
+    }
+    else {
+        throw std::runtime_error("Invalid setup type: " + cfg->camera_->get_setup_type_string());
+    }
+
+#ifdef USE_GOOGLE_PERFTOOLS
+    ProfilerStop();
+#endif
+
+    return EXIT_SUCCESS;
+}
