db6293be9dd649f12ada74e15f45b624b19684e4
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2019-09-26 16:41:18 +0900

    Add comment to orb_extractor_node.h

--- src/openvslam/feature/orb_extractor_node.h
+++ src/openvslam/feature/orb_extractor_node.h
@@ -10,16 +10,22 @@ namespace feature {
 
 class orb_extractor_node {
 public:
+    //! Constructor
     orb_extractor_node() = default;
 
+    //! Devide node to four child nodes
     std::array<orb_extractor_node, 4> divide_node();
 
+    //! Keypoints which distributed into this node
     std::vector<cv::KeyPoint> keypts_;
 
+    //! Begin and end of allocated area on the image
     cv::Point2i pt_begin_, pt_end_;
 
+    //! A iterator pointing to self, used for removal on list
     std::list<orb_extractor_node>::iterator iter_;
 
+    //! A flag designating if this node is leaf node
     bool is_leaf_node_ = false;
 };
 
