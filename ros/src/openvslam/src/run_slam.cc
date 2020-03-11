3ebcad17925d38ebe00b170a5833d2d34f1e0569
Paul Lesur <git@lesurpaul.fr>
2020-03-11 16:02:54 +0100

    add support for stereo cameras in live ros wrapper

--- ros/src/openvslam/src/run_slam.cc
+++ ros/src/openvslam/src/run_slam.cc
@@ -6,6 +6,7 @@
 
 #include <openvslam/system.h>
 #include <openvslam/config.h>
+#include <openvslam/util/stereo_rectifier.h>
 
 #include <iostream>
 #include <chrono>
@@ -13,6 +14,8 @@
 
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+#include <message_filters/sync_policies/approximate_time.h>
 #include <cv_bridge/cv_bridge.h>
 
 #include <opencv2/core/core.hpp>
@@ -66,6 +69,130 @@ void mono_tracking(const std::shared_ptr<openvslam::config>& cfg, const std::str
         const auto track_time = std::chrono::duration_cast<std::chrono::duration<double>>(tp_2 - tp_1).count();
         track_times.push_back(track_time);
     });
+
+    // run the viewer in another thread
+#ifdef USE_PANGOLIN_VIEWER
+    std::thread thread([&]() {
+        viewer.run();
+        if (SLAM.terminate_is_requested()) {
+            // wait until the loop BA is finished
+            while (SLAM.loop_BA_is_running()) {
+                std::this_thread::sleep_for(std::chrono::microseconds(5000));
+            }
+            ros::shutdown();
+        }
+    });
+#elif USE_SOCKET_PUBLISHER
+    std::thread thread([&]() {
+        publisher.run();
+        if (SLAM.terminate_is_requested()) {
+            // wait until the loop BA is finished
+            while (SLAM.loop_BA_is_running()) {
+                std::this_thread::sleep_for(std::chrono::microseconds(5000));
+            }
+            ros::shutdown();
+        }
+    });
+#endif
+
+    ros::spin();
+
+    // automatically close the viewer
+#ifdef USE_PANGOLIN_VIEWER
+    viewer.request_terminate();
+    thread.join();
+#elif USE_SOCKET_PUBLISHER
+    publisher.request_terminate();
+    thread.join();
+#endif
+
+    // shutdown the SLAM process
+    SLAM.shutdown();
+
+    if (eval_log) {
+        // output the trajectories for evaluation
+        SLAM.save_frame_trajectory("frame_trajectory.txt", "TUM");
+        SLAM.save_keyframe_trajectory("keyframe_trajectory.txt", "TUM");
+        // output the tracking times for evaluation
+        std::ofstream ofs("track_times.txt", std::ios::out);
+        if (ofs.is_open()) {
+            for (const auto track_time : track_times) {
+                ofs << track_time << std::endl;
+            }
+            ofs.close();
+        }
+    }
+
+    if (!map_db_path.empty()) {
+        // output the map database
+        SLAM.save_map_database(map_db_path);
+    }
+
+    if (track_times.size()) {
+        std::sort(track_times.begin(), track_times.end());
+        const auto total_track_time = std::accumulate(track_times.begin(), track_times.end(), 0.0);
+        std::cout << "median tracking time: " << track_times.at(track_times.size() / 2) << "[s]" << std::endl;
+        std::cout << "mean tracking time: " << total_track_time / track_times.size() << "[s]" << std::endl;
+    }
+}
+
+void stereo_tracking(const std::shared_ptr<openvslam::config>& cfg, const std::string& vocab_file_path,
+                     const std::string& mask_img_path, const bool eval_log, const std::string& map_db_path) {
+    // load the mask image
+    const cv::Mat mask = mask_img_path.empty() ? cv::Mat{} : cv::imread(mask_img_path, cv::IMREAD_GRAYSCALE);
+
+    const openvslam::util::stereo_rectifier rectifier(cfg);
+
+    // build a SLAM system
+    openvslam::system SLAM(cfg, vocab_file_path);
+    // startup the SLAM process
+    SLAM.startup();
+
+    // create a viewer object
+    // and pass the frame_publisher and the map_publisher
+#ifdef USE_PANGOLIN_VIEWER
+    pangolin_viewer::viewer viewer(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
+#elif USE_SOCKET_PUBLISHER
+    socket_publisher::publisher publisher(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
+#endif
+
+    std::vector<double> track_times;
+    const auto tp_0 = std::chrono::steady_clock::now();
+
+    // initialize this node
+    const ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+
+    image_transport::SubscriberFilter left(it, "/camera_left/image_raw", 1);
+    image_transport::SubscriberFilter right(it, "/camera_right/image_raw", 1);
+    using SyncPolicy = message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image>;
+    auto sync = message_filters::Synchronizer(SyncPolicy(10), left, right);
+
+    cv::Mat leftcv_rect;
+    cv::Mat rightcv_rect;
+    auto callback = [&](const sensor_msgs::ImageConstPtr& left,
+                        const sensor_msgs::ImageConstPtr& right) {
+        auto leftcv = cv_bridge::toCvShare(left, "mono8")->image;
+        auto rightcv = cv_bridge::toCvShare(right, "mono8")->image;
+        if (leftcv.empty() || rightcv.empty()) {
+            return;
+        }
+
+        rectifier.rectify(leftcv, rightcv, leftcv_rect, rightcv_rect);
+
+        const auto tp_1 = std::chrono::steady_clock::now();
+        const auto timestamp = std::chrono::duration_cast<std::chrono::duration<double>>(tp_1 - tp_0).count();
+
+        // input the current frame and estimate the camera pose
+        SLAM.feed_stereo_frame(leftcv_rect, rightcv_rect, timestamp, mask);
+
+        const auto tp_2 = std::chrono::steady_clock::now();
+
+        const auto track_time = std::chrono::duration_cast<std::chrono::duration<double>>(tp_2 - tp_1).count();
+        track_times.push_back(track_time);
+    };
+
+    sync.registerCallback(boost::bind<void>(callback, _1, _2));
 
     // run the viewer in another thread
 #ifdef USE_PANGOLIN_VIEWER
@@ -197,6 +324,9 @@ int main(int argc, char* argv[]) {
     // run tracking
     if (cfg->camera_->setup_type_ == openvslam::camera::setup_type_t::Monocular) {
         mono_tracking(cfg, vocab_file_path->value(), mask_img_path->value(), eval_log->is_set(), map_db_path->value());
+    }
+    else if (cfg->camera_->setup_type_ == openvslam::camera::setup_type_t::Stereo) {
+        stereo_tracking(cfg, vocab_file_path->value(), mask_img_path->value(), eval_log->is_set(), map_db_path->value());
     }
     else {
         throw std::runtime_error("Invalid setup type: " + cfg->camera_->get_setup_type_string());
