3208e79dcbad8d896a8b580c3f7e0f249cf323e2
ymd-stella <world.applepie@gmail.com>
2020-03-15 01:58:01 +0900

    Add openvslam_ros

---
+++ ros/src/openvslam/src/openvslam_ros.cc
@@ -0,0 +1,64 @@
+#include <openvslam_ros.h>
+
+#include <chrono>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+namespace openvslam_ros {
+system::system(const std::shared_ptr<openvslam::config>& cfg, const std::string& vocab_file_path, const std::string& mask_img_path)
+    : SLAM_(cfg, vocab_file_path), cfg_(cfg), it_(image_transport::ImageTransport(nh_)), tp_0_(std::chrono::steady_clock::now()),
+      mask_(mask_img_path.empty() ? cv::Mat{} : cv::imread(mask_img_path, cv::IMREAD_GRAYSCALE)) {}
+
+mono::mono(const std::shared_ptr<openvslam::config>& cfg, const std::string& vocab_file_path, const std::string& mask_img_path)
+    : system(cfg, vocab_file_path, mask_img_path) {
+    sub_ = it_.subscribe("camera/image_raw", 1, &mono::callback, this);
+}
+void mono::callback(const sensor_msgs::ImageConstPtr& msg) {
+    const auto tp_1 = std::chrono::steady_clock::now();
+    const auto timestamp = std::chrono::duration_cast<std::chrono::duration<double>>(tp_1 - tp_0_).count();
+
+    // input the current frame and estimate the camera pose
+    auto encoding = (cfg_->camera_->color_order_ == openvslam::camera::color_order_t::Gray) ? "mono8" : "bgr8";
+    SLAM_.feed_monocular_frame(cv_bridge::toCvShare(msg, encoding)->image, timestamp, mask_);
+
+    const auto tp_2 = std::chrono::steady_clock::now();
+
+    const auto track_time = std::chrono::duration_cast<std::chrono::duration<double>>(tp_2 - tp_1).count();
+    track_times_.push_back(track_time);
+}
+
+stereo::stereo(const std::shared_ptr<openvslam::config>& cfg, const std::string& vocab_file_path, const std::string& mask_img_path,
+               const bool rectify)
+    : system(cfg, vocab_file_path, mask_img_path),
+      rectifier_(rectify ? std::make_shared<openvslam::util::stereo_rectifier>(cfg) : nullptr),
+      left_sf_(image_transport::SubscriberFilter(it_, "/camera_left/image_raw", 1)),
+      right_sf_(image_transport::SubscriberFilter(it_, "/camera_right/image_raw", 1)),
+      sync_(message_filters::Synchronizer(SyncPolicy(10), left_sf_, right_sf_)) {
+    sync_.registerCallback(boost::bind<void>(&stereo::callback, this, _1, _2));
+}
+
+void stereo::callback(const sensor_msgs::ImageConstPtr& left, const sensor_msgs::ImageConstPtr& right) {
+    auto encoding = (cfg_->camera_->color_order_ == openvslam::camera::color_order_t::Gray) ? "mono8" : "bgr8";
+    auto leftcv = cv_bridge::toCvShare(left, encoding)->image;
+    auto rightcv = cv_bridge::toCvShare(right, encoding)->image;
+    if (leftcv.empty() || rightcv.empty()) {
+        return;
+    }
+
+    if (rectifier_) {
+        rectifier_->rectify(leftcv, rightcv, leftcv, rightcv);
+    }
+
+    const auto tp_1 = std::chrono::steady_clock::now();
+    const auto timestamp = std::chrono::duration_cast<std::chrono::duration<double>>(tp_1 - tp_0_).count();
+
+    // input the current frame and estimate the camera pose
+    SLAM_.feed_stereo_frame(leftcv, rightcv, timestamp, mask_);
+
+    const auto tp_2 = std::chrono::steady_clock::now();
+
+    const auto track_time = std::chrono::duration_cast<std::chrono::duration<double>>(tp_2 - tp_1).count();
+    track_times_.push_back(track_time);
+}
+} // namespace openvslam_ros
---
+++ ros/src/openvslam/src/openvslam_ros.h
@@ -0,0 +1,50 @@
+#ifndef OPENVSLAM_ROS_H
+#define OPENVSLAM_ROS_H
+
+#include <openvslam/system.h>
+#include <openvslam/config.h>
+#include <openvslam/util/stereo_rectifier.h>
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include <opencv2/core/core.hpp>
+
+namespace openvslam_ros {
+class system {
+public:
+    system(const std::shared_ptr<openvslam::config>& cfg, const std::string& vocab_file_path, const std::string& mask_img_path);
+    openvslam::system SLAM_;
+    std::shared_ptr<openvslam::config> cfg_;
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    decltype(std::chrono::steady_clock::now()) tp_0_;
+    cv::Mat mask_;
+    std::vector<double> track_times_;
+};
+
+class mono : public system {
+public:
+    mono(const std::shared_ptr<openvslam::config>& cfg, const std::string& vocab_file_path, const std::string& mask_img_path);
+    void callback(const sensor_msgs::ImageConstPtr& msg);
+
+    image_transport::Subscriber sub_;
+};
+
+class stereo : public system {
+public:
+    stereo(const std::shared_ptr<openvslam::config>& cfg, const std::string& vocab_file_path, const std::string& mask_img_path,
+           const bool rectify);
+    void callback(const sensor_msgs::ImageConstPtr& left, const sensor_msgs::ImageConstPtr& right);
+
+    std::shared_ptr<openvslam::util::stereo_rectifier> rectifier_;
+    image_transport::SubscriberFilter left_sf_, right_sf_;
+    using SyncPolicy = message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image>;
+    message_filters::Synchronizer<SyncPolicy> sync_;
+};
+} // namespace openvslam_ros
+
+#endif // OPENVSLAM_ROS_H
