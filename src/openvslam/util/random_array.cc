2512b9cddd9e271c77bb4429c0131327522cffb9
shinsumicco <shinsumicco@gmail.com>
2020-06-02 23:35:08 +0900

    Translate comments in util/random_array.cc

--- src/openvslam/util/random_array.cc
+++ src/openvslam/util/random_array.cc
@@ -22,42 +22,42 @@ std::vector<T> create_random_array(const size_t size, const T rand_min, const T 
     assert(rand_min <= rand_max);
     assert(size <= static_cast<size_t>(rand_max - rand_min + 1));
 
-    // メルセンヌ・ツイスタ作成
+    // Generate Mersenne-Twister
     auto random_engine = create_random_engine();
     std::uniform_int_distribution<T> uniform_int_distribution(rand_min, rand_max);
 
-    // sizeより少し大きくランダム数列(重複あり)を作成する
+    // Create a random sequence slightly larger than the 'size' (because duplication possibly exists)
     const auto make_size = static_cast<size_t>(size * 1.2);
 
-    // vがsizeになるまで繰り返す
+    // Iterate until the output vector reaches the 'size'
     std::vector<T> v;
     v.reserve(size);
     while (v.size() != size) {
-        // ランダム整数列を順に追加(重複がある可能性がある)
+        // Add random integer values
         while (v.size() < make_size) {
             v.push_back(uniform_int_distribution(random_engine));
         }
 
-        // ソートして重複を除く -> 重複が除かれた数列の末尾のイテレータがunique_endに入る
+        // Sort to remove duplicates so that the last iterator of the deduplicated sequence goes into 'unique_end'
         std::sort(v.begin(), v.end());
         auto unique_end = std::unique(v.begin(), v.end());
 
-        // vのサイズが大きすぎたら，sizeまでのイテレータに変えておく
+        // If the vector size is too large, change it to an iterator up to the 'size'
         if (size < static_cast<size_t>(std::distance(v.begin(), unique_end))) {
             unique_end = std::next(v.begin(), size);
         }
 
-        // 重複部分から最後までを削除する
+        // Delete the portion from the duplication to the end
         v.erase(unique_end, v.end());
     }
 
-    // 昇順になっているのでシャッフル
+    // Shuffle because they are in ascending order
     std::shuffle(v.begin(), v.end(), random_engine);
 
     return v;
 }
 
-// 明示的に実体化しておく
+// template specialization
 template std::vector<int> create_random_array(size_t, int, int);
 template std::vector<unsigned int> create_random_array(size_t, unsigned int, unsigned int);
 
