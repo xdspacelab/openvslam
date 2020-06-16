91bdbd7b129cee3b4661d017ac20897b44ce4405
shinsumicco <shinsumicco@gmail.com>
2020-06-16 22:12:54 +0900

    Translate comments in solve/sim3_solver.cc

--- src/openvslam/solve/sim3_solver.cc
+++ src/openvslam/solve/sim3_solver.cc
@@ -17,16 +17,16 @@ sim3_solver::sim3_solver(data::keyframe* keyfrm_1, data::keyframe* keyfrm_2,
                          const bool fix_scale, const unsigned int min_num_inliers)
     : keyfrm_1_(keyfrm_1), keyfrm_2_(keyfrm_2),
       fix_scale_(fix_scale), min_num_inliers_(min_num_inliers) {
-    // keyframe1の3次元点
+    // 3D points seen in the current keyframe (keyframe 1)
     const auto keyfrm_1_lms = keyfrm_1_->get_landmarks();
 
-    // 3次元点を各カメラ姿勢基準に変換するので姿勢を出しておく
+    // Get camera poses in order to convert 3D points to the coordinates whose reference is each of the cameras
     const Mat33_t rot_1w = keyfrm_1_->get_rotation();
     const Vec3_t trans_1w = keyfrm_1_->get_translation();
     const Mat33_t rot_2w = keyfrm_2_->get_rotation();
     const Vec3_t trans_2w = keyfrm_2_->get_translation();
 
-    // メモリを確保
+    // Pre-allocate memory
     {
         const auto size = std::min(keyfrm_1_lms.size(), matched_lms_in_keyfrm_2.size());
         common_pts_in_keyfrm_1_.reserve(size);
@@ -37,7 +37,7 @@ sim3_solver::sim3_solver(data::keyframe* keyfrm_1, data::keyframe* keyfrm_2,
         matched_indices_2_.reserve(size);
     }
 
-    // 有意水準1%のカイ2乗値(自由度2)
+    // Chi-squared value with significance level of 1% (n=2)
     constexpr float chi_sq_2D = 9.21034;
 
     num_common_pts_ = 0;
@@ -46,8 +46,8 @@ sim3_solver::sim3_solver(data::keyframe* keyfrm_1, data::keyframe* keyfrm_2,
             continue;
         }
 
-        auto* lm_1 = keyfrm_1_lms.at(idx1);
-        auto* lm_2 = matched_lms_in_keyfrm_2.at(idx1);
+        auto lm_1 = keyfrm_1_lms.at(idx1);
+        auto lm_2 = matched_lms_in_keyfrm_2.at(idx1);
 
         if (!lm_1 || !lm_2) {
             continue;
@@ -88,7 +88,7 @@ sim3_solver::sim3_solver(data::keyframe* keyfrm_1, data::keyframe* keyfrm_2,
 }
 
 void sim3_solver::find_via_ransac(const unsigned int max_num_iter) {
-    // best modelを初期化
+    // Initialize the best model
     unsigned int max_num_inliers = 0;
     solution_is_valid_ = false;
     best_rot_12_ = Mat33_t::Zero();
@@ -100,7 +100,7 @@ void sim3_solver::find_via_ransac(const unsigned int max_num_iter) {
         return;
     }
 
-    // RANSAC loop内で使用する変数
+    // Variables used in the RANSAC loop
     Mat33_t rot_12_in_sac;
     Vec3_t trans_12_in_sac;
     float scale_12_in_sac;
@@ -110,7 +110,7 @@ void sim3_solver::find_via_ransac(const unsigned int max_num_iter) {
 
     // RANSAC loop
     for (unsigned int iter = 0; iter < max_num_iter; ++iter) {
-        // 3次元点を3つランダムサンプリングして行列にする
+        // Randomly sample three 3D points into a matrix
         Mat33_t pts_1, pts_2;
         const auto random_indices = util::create_random_array(3, 0, static_cast<int>(num_common_pts_ - 1));
         for (unsigned int i = 0; i < 3; ++i) {
@@ -118,18 +118,18 @@ void sim3_solver::find_via_ransac(const unsigned int max_num_iter) {
             pts_2.block(0, i, 3, 1) = common_pts_in_keyfrm_2_.at(random_indices.at(i));
         }
 
-        // 相似変換行列(R,t,s)を求める
+        // Compute the similarity transformation matrix (R, t, s)
         compute_Sim3(pts_1, pts_2,
                      rot_12_in_sac, trans_12_in_sac, scale_12_in_sac,
                      rot_21_in_sac, trans_21_in_sac, scale_21_in_sac);
 
-        // インライアを数える
+        // Count the inliers
         std::vector<bool> inliers;
         const auto num_inliers = count_inliers(rot_12_in_sac, trans_12_in_sac, scale_12_in_sac,
                                                rot_21_in_sac, trans_21_in_sac, scale_21_in_sac,
                                                inliers);
 
-        // ベストモデルを更新
+        // Update the best model
         if (max_num_inliers < num_inliers) {
             max_num_inliers = num_inliers;
             best_rot_12_ = rot_12_in_sac;
@@ -139,7 +139,7 @@ void sim3_solver::find_via_ransac(const unsigned int max_num_iter) {
     }
 
     if (max_num_inliers < min_num_inliers_) {
-        // インライア数の最小条件を満たせなかった場合は推定失敗
+        // Estimation fails if the number of the inliers is insufficient for the minimal condition
         solution_is_valid_ = false;
         best_rot_12_ = Mat33_t::Zero();
         best_trans_12_ = Vec3_t::Zero();
@@ -158,11 +158,11 @@ void sim3_solver::compute_Sim3(const Mat33_t& pts_1, const Mat33_t& pts_2,
     // Based on "Closed-form solution of absolute orientation using unit quaternions"
     // http://people.csail.mit.edu/bkph/papers/Absolute_Orientation.pdf
 
-    // 各点集合のcentroidを求める
+    // Compute the centroid of each point set
     const Vec3_t centroid_1 = pts_1.rowwise().mean();
     const Vec3_t centroid_2 = pts_2.rowwise().mean();
 
-    // 分布の中心をcentroidに動かす
+    // Move the center of the distribution to the centroid
     Mat33_t ave_pts_1 = pts_1;
     ave_pts_1.colwise() -= centroid_1;
     Mat33_t ave_pts_2 = pts_2;
@@ -170,10 +170,10 @@ void sim3_solver::compute_Sim3(const Mat33_t& pts_1, const Mat33_t& pts_2,
 
     // 4.A Matrix of Sums of Products
 
-    // 行列Mを求める
+    // Compute the matrix M
     const Mat33_t M = ave_pts_1 * ave_pts_2.transpose();
 
-    // 行列Nを求める
+    // Compute the matrix N
     const double& Sxx = M(0, 0);
     const double& Syx = M(1, 0);
     const double& Szx = M(2, 0);
@@ -191,10 +191,10 @@ void sim3_solver::compute_Sim3(const Mat33_t& pts_1, const Mat33_t& pts_2,
 
     // 4.B Eigenvector Maximizes Matrix Product
 
-    // Nを固有値分解する
+    // Find the eigenvectors of the matrix N
     Eigen::EigenSolver<Mat44_t> eigensolver(N);
 
-    // 最大固有値を探す
+    // Find a maximum eigenvalue
     const auto& eigenvalues = eigensolver.eigenvalues();
     int max_idx = -1;
     double max_eigenvalue = -INFINITY;
@@ -206,15 +206,15 @@ void sim3_solver::compute_Sim3(const Mat33_t& pts_1, const Mat33_t& pts_2,
     }
     const auto max_eigenvector = eigensolver.eigenvectors().col(max_idx);
 
-    // 複素数なので実数のみ取り出す
+    // Extract only real numbers since eigenvalues are contained as complex numbers
     Eigen::Vector4d eigenvector;
     eigenvector << max_eigenvector(0, 0).real(), max_eigenvector(1, 0).real(), max_eigenvector(2, 0).real(), max_eigenvector(3, 0).real();
     eigenvector.normalize();
 
-    // unit quaternionにする
+    // Create a unit quaternion with the eigenvector correspounding to the maximum eigenvalue
     Eigen::Quaterniond q_rot_21(eigenvector(0), eigenvector(1), eigenvector(2), eigenvector(3));
 
-    // 回転行列に変換
+    // Convert to a rotation matrix
     rot_21 = q_rot_21.normalized().toRotationMatrix();
 
     // 2.D Finding the Scale
@@ -223,14 +223,14 @@ void sim3_solver::compute_Sim3(const Mat33_t& pts_1, const Mat33_t& pts_2,
         scale_21 = 1.0;
     }
     else {
-        // averaged points 1をpoints 2の座標系に変換(回転のみ)
+        // Convert the points 1 to the coordinate system for the points 2 (rotation only)
         const Mat33_t ave_pts_1_in_2 = rot_21 * ave_pts_1;
 
-        // 分母
+        // Denominator
         const double denom = ave_pts_1.squaredNorm();
-        // 分子
+        // Numerator
         const double numer = ave_pts_2.cwiseProduct(ave_pts_1_in_2).sum();
-        // スケール
+        // Scale
         scale_21 = numer / denom;
     }
 
@@ -238,7 +238,7 @@ void sim3_solver::compute_Sim3(const Mat33_t& pts_1, const Mat33_t& pts_2,
 
     trans_21 = centroid_2 - scale_21 * rot_21 * centroid_1;
 
-    // 逆変換する
+    // Transpose
     rot_12 = rot_21.transpose();
     scale_12 = 1.0 / scale_21;
     trans_12 = -scale_12 * rot_12 * trans_21;
@@ -247,28 +247,29 @@ void sim3_solver::compute_Sim3(const Mat33_t& pts_1, const Mat33_t& pts_2,
 unsigned int sim3_solver::count_inliers(const Mat33_t& rot_12, const Vec3_t& trans_12, const float scale_12,
                                         const Mat33_t& rot_21, const Vec3_t& trans_21, const float scale_21,
                                         std::vector<bool>& inliers) {
-    // 推定した相似変換行列を使って，片方の3次元点をもう片方の画像上に再投影し，距離計算する
+    // Reproject the 3D points seen in one image onto the other image using the estimated similarity transformation matrix
+    // and then compute the distance
     unsigned int num_inliers = 0;
     inliers.resize(num_common_pts_, false);
 
-    // 座標系1の3次元点を座標系2の画像上に投影
+    // Reroject the 3D points of coordinate system 1 onto the image of coordinate system 2
     std::vector<Vec2_t, Eigen::aligned_allocator<Vec2_t>> reprojected_1_in_cam_2;
     reproject_to_other_image(common_pts_in_keyfrm_1_, reprojected_1_in_cam_2, rot_21, trans_21, scale_21, keyfrm_2_);
 
-    // 座標系2の3次元点を座標系1の画像上に投影
+    // Reroject the 3D points of coordinate system 2 onto the image of coordinate system 1
     std::vector<Vec2_t, Eigen::aligned_allocator<Vec2_t>> reprojected_2_in_cam_1;
     reproject_to_other_image(common_pts_in_keyfrm_2_, reprojected_2_in_cam_1, rot_12, trans_12, scale_12, keyfrm_1_);
 
     for (unsigned int i = 0; i < num_common_pts_; ++i) {
-        // 残差ベクトルを計算
+        // Compute the residual vector
         const Vec2_t dist_in_2 = (reprojected_1_in_cam_2.at(i) - reprojected_2_.at(i));
         const Vec2_t dist_in_1 = (reprojected_2_in_cam_1.at(i) - reprojected_1_.at(i));
 
-        // 2乗誤差を計算
+        // Compute squared error
         const double error_in_2 = dist_in_2.dot(dist_in_2);
         const double error_in_1 = dist_in_1.dot(dist_in_1);
 
-        // インライアチェック
+        // Inlier check
         if (error_in_2 < chi_sq_x_sigma_sq_2_.at(i) && error_in_1 < chi_sq_x_sigma_sq_1_.at(i)) {
             inliers.at(i) = true;
             ++num_inliers;
