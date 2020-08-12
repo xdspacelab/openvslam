6f4160bd4e10a9457442a2c69b05b6e54ba43bb6
shinsumicco <shinsumicco@gmail.com>
2020-08-12 21:39:08 +0900

    Show all parameters written in YAML

--- src/openvslam/config.cc
+++ src/openvslam/config.cc
@@ -3,6 +3,7 @@
 #include "openvslam/camera/fisheye.h"
 #include "openvslam/camera/equirectangular.h"
 #include "openvslam/camera/radial_division.h"
+#include "openvslam/util/string.h"
 
 #include <iostream>
 #include <memory>
@@ -132,6 +133,41 @@ std::ostream& operator<<(std::ostream& os, const config& cfg) {
     if (cfg.camera_->setup_type_ == camera::setup_type_t::RGBD) {
         std::cout << "Depth Image Configuration:" << std::endl;
         std::cout << "- depthmap factor: " << cfg.depthmap_factor_ << std::endl;
+    }
+
+    const std::vector<std::string> shown_entries = {"Camera", "Feature", "depth_threshold", "depthmap_factor"};
+
+    // extract entries which have not shown yet
+    std::map<std::string, YAML::Node> remained_nodes;
+    for (const auto& kv : cfg.yaml_node_) {
+        const auto entry = kv.first.as<std::string>();
+        // avoid displaying parameters twice
+        const bool entry_was_shown = std::any_of(shown_entries.begin(), shown_entries.end(),
+                                                 [&entry](const std::string& qry) { return util::string_startswith(entry, qry); });
+        if (entry_was_shown) {
+            continue;
+        }
+        remained_nodes[entry] = kv.second;
+    }
+
+    std::cout << "Other Configuration:" << std::endl;
+    std::string prev_entry = "";
+    for (const auto& node : remained_nodes) {
+        const auto& entry = node.first;
+        // split with '.'
+        const auto splitted_entry = util::split_string(entry, '.');
+        // show parameter hierarchically
+        if (prev_entry != splitted_entry.at(0)) {
+            std::cout << "  " << splitted_entry.at(0) << ":" << std::endl;
+        }
+        prev_entry = splitted_entry.at(0);
+        std::cout << "  - " << splitted_entry.at(1) << ": ";
+        try {
+            std::cout << cfg.yaml_node_[entry].as<std::string>() << std::endl;
+        }
+        catch (const std::exception& e) {
+            std::cout << cfg.yaml_node_[entry] << std::endl;
+        }
     }
 
     return os;
---
+++ src/openvslam/util/string.h
@@ -0,0 +1,31 @@
+#ifndef OPENVSLAM_UTIL_STRING_H
+#define OPENVSLAM_UTIL_STRING_H
+
+#include <vector>
+#include <string>
+#include <sstream>
+
+namespace openvslam {
+namespace util {
+
+inline std::vector<std::string> split_string(const std::string& str, const char del) {
+    std::vector<std::string> splitted_strs;
+    std::stringstream ss(str);
+    std::string item;
+    while (std::getline(ss, item, del)) {
+        if (!item.empty()) {
+            splitted_strs.push_back(item);
+        }
+    }
+    return splitted_strs;
+}
+
+inline bool string_startswith(const std::string& str, const std::string& qry) {
+    return str.size() >= qry.size()
+           && std::equal(std::begin(qry), std::end(qry), std::begin(str));
+}
+
+} // namespace util
+} // namespace openvslam
+
+#endif // OPENVSLAM_UTIL_STRING_H
